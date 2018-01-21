d01283f29fc7e5617e96af751c109cb92d666d40
Alexandre Lision <alexandre.lision@gmail.com>
2018-01-21 22:07:59 +0000

    Add missing shebang and comments

--- setup-mirror.sh
+++ setup-mirror.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+
 sudo wget http://node-arm.herokuapp.com/node_latest_armhf.deb
 sudo dpkg -i node_latest_armhf.deb
 sudo apt-get install -y npm
 sudo apt-get install -y git
+
 cd /home/pi/
 git clone https://github.com/MichMich/MagicMirror
 cd MagicMirror
@@ -10,6 +13,8 @@ sudo npm install #This will take a LONG time, expect 30 minutes
 sudo apt-get install -y unclutter
 sudo aptitude install -y xinit
 sudo apt-get install -y matchbox
+
+# THis step needs to be done manually
 sudo raspi-config #-> Boot Options -> B1 Desktop/CLI -> B2 Console Autologin
 
 sudo cat >> /home/pi/start.sh << EOF
@@ -20,6 +25,7 @@ sleep 45
 sudo xinit /home/pi/startDisplay.sh
 EOF
 
+# Place the start.sh script in the init.d dir for automatic launch on boot
 sudo chmod a+x /home/pi/start.sh
 sudo mv /home/pi/start.sh /etc/init.d/startMagicMirror.sh
 sudo update-rc.d startMagicMirror.sh defaults 100
